6372018a5f102798f1df830c0aa0d1c8fafb2198
Alex Perez <alejandro.gperez745@gmail.com>
2023-03-06 20:01:13 -0600

    code cleanup and added github icon

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
-const top4List = document.querySelector('.top4')
-const randomList = document.querySelector('.random')
-const genreFilter = document.querySelector('.genre')
-const platformFilter = document.querySelector('.platform')
+const top4List = document.querySelector('#top4')
+const randomList = document.querySelector('#random')
+const genreFilter = document.querySelector('#genreFilter')
+const platformFilter = document.querySelector('#platformFilter')
 const ratingFilter = document.querySelector('#ratingInput')
 const startDateFilter = document.querySelector('#startDateInput')
 const endDateFilter = document.querySelector('#endDateInput')
-const filters = document.getElementById('filters')
-const hideShowBox = document.getElementById('hideShowBox')
+const filters = document.querySelector('#filters')
+const hideShowBox = document.querySelector('#hideShowBox')
 
 hotGames(top4List)
 generalGames(randomList)
 getGenres(genreFilter)
 getPlatforms(platformFilter)
 
+
+//make dynamic/ reusable
 hideShowBox.addEventListener('change',function(){
     if (this.checked) {
         filters.setAttribute("style","display:block") 
